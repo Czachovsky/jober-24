658039140e590238ddc47430b406e64d962394d3
Marcin Rajchel <rajchel.marcin@gmail.com>
2025-03-08 23:08:36 +0100

    licznik + nowe mapy + terms + privacy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,26 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, inject} from '@angular/core';
+import {NavigationEnd, Router, RouterOutlet} from '@angular/router';
 import {HeaderComponent} from "./sections/header/header.component";
-import {HeroComponent} from "./sections/hero/hero.component";
+
 import {FooterComponent} from "./sections/footer/footer.component";
-import {TestimonialsComponent} from "./sections/testimonials/testimonials.component";
-import {ServicesComponent} from "./sections/services/services.component";
-import {TimelineComponent} from "./sections/timeline/timeline.component";
-import {LocationsComponent} from "./sections/locations/locations.component";
-import {FeaturesComponent} from "./sections/features/features.component";
 import {ButtonComponent} from "./components/button/button.component";
-import {WhyUsComponent} from "./sections/why-us/why-us.component";
-import {BenefitsComponent} from "./sections/benefits/benefits.component";
 import {ToastComponent} from "./components/toast/toast.component";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, HeroComponent, FooterComponent, TestimonialsComponent, ServicesComponent, TimelineComponent, LocationsComponent, FeaturesComponent, ButtonComponent, WhyUsComponent, BenefitsComponent, ToastComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, ButtonComponent, ToastComponent],
   templateUrl: './app.component.html'
 })
 export class AppComponent {
+  private router = inject(Router);
+
+  constructor() {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        window.scrollTo({top: 0, behavior: 'smooth'});
+      }
+    });
+  }
 }
